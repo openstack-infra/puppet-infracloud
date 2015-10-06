e7a51725e7c69782dcc8bbe4c7030a4e0cd7725d
Colleen Murphy <colleen@gazlene.net>
2015-10-06 16:33:47 -0700

    Add bifrost manifest
    Add class to manage bifrost:
     - clones bifrost from source
     - adds config files
     - runs bifrost install procedure
     - runs custom ansible playbook to generate static dhcp inventory
     - starts an additional dnsmasq instance and runs it like a service
    
    The config templates are currently highly tailored to Infra's use case
    and may need to be reworked if we wish to make this consumable
    downstream.
    
    Based on the ironic::bifrost class[1] but with some modifications to
    avoid heavy reliance on the puppet DSL.
    
    Co-Authored-By: Clint Byrum <clint@fewbar.com>
    Co-Authored-By: greghaynes <greg@greghaynes.net>
    Co-Authored-By: Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
    Co-Authored-By: Yolanda Robla <info@ysoft.biz>
    
    [1] http://git.openstack.org/cgit/openstack/puppet-ironic/tree/manifests/bifrost.pp
    
    Change-Id: I949344c16cf9ee3965b0bc96850eb208ac65b168

---
+++ manifests/bifrost.pp
@@ -0,0 +1,106 @@
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+class infracloud::bifrost (
+  $ironic_inventory,
+  $ironic_db_password,
+  $mysql_password,
+  $region,
+  $ipmi_passwords,
+  $ssh_public_key,
+  $ssh_private_key,
+) {
+
+  # The configdrive bifrost task defaults to copying the user's local public
+  # ssh key. Let's make sure it's there so that bifrost doesn't error and so we
+  # can log in to nodes from the baremetal host.
+  file { '/root/.ssh/id_rsa':
+    ensure  => present,
+    mode    => '0600',
+    content => $ssh_private_key,
+    before  => Exec['install bifrost'],
+  }
+
+  file { '/root/.ssh/id_rsa.pub':
+    ensure  => present,
+    mode    => '0644',
+    content => $ssh_public_key,
+    before  => Exec['install bifrost'],
+  }
+
+  include ::ansible
+
+  class { '::mysql::server':
+    root_password => $mysql_password,
+  }
+
+  vcsrepo { '/opt/stack/bifrost':
+    ensure   => 'latest',
+    provider => 'git',
+    revision => 'master',
+    source   => 'https://git.openstack.org/openstack/bifrost',
+  }
+
+  file { '/etc/bifrost':
+    ensure => directory,
+  }
+
+  file { '/etc/bifrost/bifrost_global_vars':
+    ensure  => present,
+    content => template("infracloud/bifrost/bifrost_global_vars.${region}.erb"),
+  }
+
+  file { '/opt/stack/baremetal.json':
+    ensure  => file,
+    content => template("infracloud/bifrost/inventory.${region}.json.erb"),
+    require => Vcsrepo['/opt/stack/bifrost'],
+  }
+
+  package { 'uuid-runtime':
+    ensure => installed
+  }
+
+  exec { 'install bifrost dependencies':
+    command     => 'pip install -U -r /opt/stack/bifrost/requirements.txt',
+    path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin',
+    refreshonly => true,
+    subscribe   => Vcsrepo['/opt/stack/bifrost'],
+  }
+
+  file { '/opt/stack/elements':
+    ensure  => directory,
+    recurse => true,
+    source  => 'puppet:///modules/infracloud/elements',
+    require => Vcsrepo['/opt/stack/bifrost'],
+    before  => Exec['install bifrost'],
+  }
+
+  exec { 'install bifrost':
+    environment => ['BIFROST_INVENTORY_SOURCE=/opt/stack/baremetal.json', 'HOME=/root'],
+    command     => "ansible-playbook -e @/etc/bifrost/bifrost_global_vars -vvvv \
+                     -i /opt/stack/bifrost/playbooks/inventory/bifrost_inventory.py \
+                     /opt/stack/bifrost/playbooks/install.yaml \
+                     && touch /var/run/bifrost_install_succeeded",
+    path        => '/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin',
+    creates     => '/var/run/bifrost_install_succeeded',
+    timeout     => 1500,
+    require     => [
+      Exec['install bifrost dependencies'],
+      File['/etc/bifrost/bifrost_global_vars'],
+      Vcsrepo['/opt/stack/bifrost'],
+      Package['uuid-runtime'],
+      Class['::mysql::server'],
+    ],
+  }
+}
