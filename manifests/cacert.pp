9f2b92478a56d7d2875c989d5dd6faa937adc0c7
Yolanda Robla <yolanda.robla-mota@hp.com>
2016-02-10 14:36:38 +0100

    Add class to deal with cacert on infra cloud
    Change-Id: Ic620bc03f49fe30e889767b63be3712a69901535

---
+++ manifests/cacert.pp
@@ -0,0 +1,21 @@
+# adds infra cloud chain to trusted certs
+class infracloud::cacert (
+  $cacert_content,
+) {
+  file { '/usr/local/share/ca-certificates/openstack_infra_ca.crt':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    content =>  $cacert_content,
+    replace => true,
+  }
+
+  exec { 'update-ca-certificates':
+    command     => '/usr/sbin/update-ca-certificates',
+    subscribe   => [
+        File['/usr/local/share/ca-certificates/openstack_infra_ca.crt'],
+      ],
+    refreshonly => true,
+  }
+}
