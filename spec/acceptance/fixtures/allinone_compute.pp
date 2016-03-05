8befdebb417f4adb399b4240cff8b06bd20cf227
Colleen Murphy <colleen@gazlene.net>
2016-03-04 21:56:10 -0800

    (WIP) Add functional tests
    This patch adds beaker tests for an "all-in-one" node consisting of an
    OpenStack controller and a compute node cohabitating the same VM. This
    includes minor modifications to the controller and compute manifests to
    prevent the manifests from overwriting each others' configs, as well as
    a new parameter to control the libvirt domain type, since our CI
    doesn't handle KVM.
    
    Change-Id: I902598fdd0a25cd84181e9661eda7d1b1a6c1ed7
    Depends-On: I558756e4704d3b3e090db92edfeca6f75def1e04

---
+++ spec/acceptance/fixtures/allinone_compute.pp
@@ -0,0 +1,29 @@
+# All-in-one - compute
+# Apply this second
+
+#subject=/C=AU/ST=Some-State/O=OpenStack Infra CI/CN=infracloud.local
+$ssl_cert_file_contents = '-----BEGIN CERTIFICATE-----
+MIICPTCCAaYCCQDuKIXOrdO/WTANBgkqhkiG9w0BAQsFADBjMQswCQYDVQQGEwJB
+VTETMBEGA1UECAwKU29tZS1TdGF0ZTEkMCIGA1UECgwbT3BlblN0YWNrIEluZnJh
+c3RydWN0dXJlIENJMRkwFwYDVQQDDBBpbmZyYWNsb3VkLmxvY2FsMB4XDTE2MDMw
+ODA0NDg0NVoXDTE2MDQwNzA0NDg0NVowYzELMAkGA1UEBhMCQVUxEzARBgNVBAgM
+ClNvbWUtU3RhdGUxJDAiBgNVBAoMG09wZW5TdGFjayBJbmZyYXN0cnVjdHVyZSBD
+STEZMBcGA1UEAwwQaW5mcmFjbG91ZC5sb2NhbDCBnzANBgkqhkiG9w0BAQEFAAOB
+jQAwgYkCgYEAuQ7BdJn0l4V0IAC9+81m4UVOH0WDcL8NIcey0kMfj7wwfoRC3bZv
+MoqT3JYSUJw7cAaKIh0YcAHsZ5XSFTOxeej6MlBOwerUlEUB8fIpWRCu3yNtgIBL
+xH1cRkHBcWo4cimpT0HkzrqlbeXGhLpWq21fR3CX7BYU+Fm6YNtE+GUCAwEAATAN
+BgkqhkiG9w0BAQsFAAOBgQBTxjRBlOqXPKXL/WGGLeM4nWcbj+c/3ebHbQB/dPEC
+NyH66SFu5Ncsu2y/Ufz2GgFUrnayNsjWEXCGLp+iGztnNZSZ1A+huGNUrwcTojf3
+/KXYEu5gjSqfM3EjoLn7I0bqHPM1H60AMezv2gqkn5CSLt2pjKiNd1BrPnKfaRTk
+Cw==
+-----END CERTIFICATE-----'
+
+class { '::infracloud::compute':
+  nova_rabbit_password          => 'XXX',
+  neutron_rabbit_password       => 'XXX',
+  neutron_admin_password        => 'XXX',
+  ssl_cert_file_contents        => $ssl_cert_file_contents,
+  br_name                       => 'br-vlan2',
+  controller_public_address     => 'localhost',
+  virt_type                     => 'qemu',
+}
