4d57ef13669c31e16232c8ac64f815be744db44b
Yolanda Robla Mota <yroblamo@redhat.com>
2016-08-24 11:55:43 +0200

    Parameterize certificates in infracloud
    SSL cert path and command are different from Debian/Redhat.
    So parameterize it to be able to reuse on different components.
    
    Change-Id: I8c1332e36f9b6a288a5736bee2a7171476aab739

---
+++ manifests/params.pp
@@ -0,0 +1,16 @@
+# common parameters to be reused in infracloud
+class infracloud::params {
+  case $::osfamily {
+    'Debian': {
+      $cert_path = '/usr/local/share/ca-certificates'
+      $cert_command = '/usr/bin/update-ca-certificates'
+    }
+    'Redhat': {
+      $cert_path = '/etc/pki/ca-trust/source/anchors'
+      $cert_command = '/usr/bin/update-ca-trust'
+    }
+    default: {
+        fail("Only Debian and RedHat distros are supported.")
+    }
+  }
+}
